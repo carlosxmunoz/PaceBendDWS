deace319690ba2e2e2d41ae930fa31fdb3afcfa1
Carlos Munoz <carlosxmunoz@protonmail.com>
2020-08-02 20:23:45 -0500

    Updates

---
+++ precache-manifest.02fabc6fe1e5e439428b5967d5b2b48c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf395fdcfa52eb477bf9a250bd556852",
+    "url": "/PaceBendDWS/index.html"
+  },
+  {
+    "revision": "d345f156630c97bf1aa9",
+    "url": "/PaceBendDWS/static/css/main.47a9c96c.chunk.css"
+  },
+  {
+    "revision": "f34221012ffbb030f1ff",
+    "url": "/PaceBendDWS/static/js/2.7d21326f.chunk.js"
+  },
+  {
+    "revision": "d345f156630c97bf1aa9",
+    "url": "/PaceBendDWS/static/js/main.0b6d39c5.chunk.js"
+  },
+  {
+    "revision": "ca4aa7bd1565ffde1143",
+    "url": "/PaceBendDWS/static/js/runtime~main.d7c2db77.js"
+  }
+]);
